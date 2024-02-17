14f7a26d1f6d496a1d2708fa8400e1902d256b6a
nasif273 <abdullahomarn57@gmail.com>
2024-02-17 16:45:20 +0600

    nasif 3

--- app.js
+++ app.js
@@ -19,9 +19,3 @@ app.get('/greet/:name', (req, res) => {
 app.use((req, res, next) => {
   res.status(404).send('404 Not Found\n');
 });
-
-// Starting the server
-const PORT = 8000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
